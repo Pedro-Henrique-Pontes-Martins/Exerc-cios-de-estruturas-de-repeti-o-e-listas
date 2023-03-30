2199e6ab0c0b48882a1a872f651a6f5493d69411
unknown <pedrosenaitech@gmail.com>
2023-03-30 16:29:18 -0300

    Realização do exercício 2

--- Exercícios de Lista.py
+++ Exercícios de Lista.py
@@ -1,4 +1,11 @@
 #Exercício 1:
-print('Exercício 1:')
+print('Exercício 1:/n' )
 Lista1 = [1, 2, 3, 4, 5]
 print(Lista1)
+
+#Exercício 2:
+print('Exercício 2:/n')
+Lista2 = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9]
+print(Lista2)
+Lista2.reverse()
+print(Lista2)
