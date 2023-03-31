3c939efd0b5db99fe39f1b32fe4cdeed642c076e
unknown <pedrosenaitech@gmail.com>
2023-03-31 10:20:05 -0300

    Finalização do exercício 5

--- Exercícios de Lista.py
+++ Exercícios de Lista.py
@@ -34,3 +34,18 @@ def quantidadeDeConsoantes(Lista4):
     return consoantes
 print('Quantidade de consoantes:')
 print(quantidadeDeConsoantes(Lista4))
+
+#Exercício 5:
+print("Exercício 5:")
+Lista5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+ListaPar = []
+ListaImpar = []
+for i in Lista5:
+    if(Lista5[i-1]%2):
+        ListaImpar.append(Lista5[i-1])
+    else:
+        ListaPar.append(Lista5[i-1])
+
+print(Lista5)
+print(ListaPar)
+print(ListaImpar)
