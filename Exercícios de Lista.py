3023fbc7288e2e0be95d0727da5994250943ba7c
30 <pedrosenaitech@gmail.com>
2023-04-06 15:16:20 -0300

    Finalização do exercício 8

--- Exercícios de Lista.py
+++ Exercícios de Lista.py
@@ -87,7 +87,7 @@ print('Número de Alunos Aprovados: ' + str(númeroDeAlunosAprovados(AllAlunos))
 
 #Exercício 7:
 print('Exercício 7:')
-ListaEx7 = [4, 7, 8, 3]
+ListaEx7 = [4, 7, 8, 3, 5]
 
 def soma(lista):
     soma = 0
@@ -105,4 +105,24 @@ print('Lista: ' + str(ListaEx7))
 print('Soma dos Valores da Lista: ' + str(soma(ListaEx7)))
 print('Multiplicação dos Valores da Lista: ' + str(multiplicação(ListaEx7)))
 
+#Exercício 8:
+listaDeIdades = []
+listaDeAlturas = []
+for i in range(0, 5):
+    print('Insira uma idade')
+    idade = input()
+    print('Insira uma altura em metros')
+    altura = input()
+    listaDeIdades.append(idade)
+    listaDeAlturas.append(altura)
+
+listaDeIdades.reverse()
+listaDeAlturas.reverse()
+
+contador = 0
+for i in listaDeIdades:
+    print('Pessoa ' + str(contador + 1) + ':' )
+    print('Idade: ' + str(i))
+    print('Altura: '+ str(listaDeAlturas[contador]))
+    contador += 1
 
