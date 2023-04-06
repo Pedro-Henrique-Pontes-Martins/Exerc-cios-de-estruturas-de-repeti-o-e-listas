6bb4e8d484eb054eb047e989f147b55c1f0dd63c
30 <pedrosenaitech@gmail.com>
2023-04-06 14:56:05 -0300

    Finalização do exercício 7

--- Exercícios de Lista.py
+++ Exercícios de Lista.py
@@ -84,3 +84,25 @@ def númeroDeAlunosAprovados(listaDeNotas):
 
 print('Médias dos Alunos: ' + str(calcularMédiasDosAlunos(AllAlunos)))
 print('Número de Alunos Aprovados: ' + str(númeroDeAlunosAprovados(AllAlunos)))
+
+#Exercício 7:
+print('Exercício 7:')
+ListaEx7 = [4, 7, 8, 3]
+
+def soma(lista):
+    soma = 0
+    for i in lista:
+        soma += i
+    return soma
+
+def multiplicação(lista):
+    multiplicação = 1
+    for i in lista:
+        multiplicação *= i
+    return multiplicação
+
+print('Lista: ' + str(ListaEx7))
+print('Soma dos Valores da Lista: ' + str(soma(ListaEx7)))
+print('Multiplicação dos Valores da Lista: ' + str(multiplicação(ListaEx7)))
+
+
