5562c2bc0d631af383c67c0aa46aacd393926ab1
30 <pedrosenaitech@gmail.com>
2023-04-13 16:52:31 -0300

    Finalização do exercício 4 da lista de repetição.

---
+++ Exercícios de Repetição/Exercicio4.py
@@ -0,0 +1,9 @@
+populaçãoA = 80000 
+populaçãoB = 200000 
+númeroDeAnos = 0
+
+while (populaçãoB > populaçãoA):
+    populaçãoA *= 1.03
+    populaçãoB *= 1.015
+    númeroDeAnos += 1
+print (númeroDeAnos)
